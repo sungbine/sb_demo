62d7ad0bb8dd06d485dd17a0a68b9f19e98ffd9c
sungbin.kim <sungbin.kim@nhnent.com>
2019-08-30 14:31:46 +0900

    first commit

---
+++ src/main/java/com/example/sbdemo/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.sbdemo;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String hello() throws UnknownHostException {
+        return "Hello " + InetAddress.getLocalHost().getHostName();
+    }
+}

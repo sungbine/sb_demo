1a21f655c0a2d85158eaf3d57ad7025332a5a799
sungbin.kim <sungbin.kim@nhnent.com>
2019-09-02 16:18:40 +0900

    no message

--- src/main/java/com/example/sbdemo/DemoController.java
+++ src/main/java/com/example/sbdemo/DemoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+
     @GetMapping("/hello")
     public String hello() throws UnknownHostException {
         return "Hello " + InetAddress.getLocalHost().getHostName();

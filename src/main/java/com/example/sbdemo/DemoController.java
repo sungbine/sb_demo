c044c4dbf347a91627e508ed6ff5b55ea02ae783
Sungbin Kim <therichbin@gmail.com>
2019-09-06 17:37:51 +0900

    Update DemoController.java

--- src/main/java/com/example/sbdemo/DemoController.java
+++ src/main/java/com/example/sbdemo/DemoController.java
@@ -11,6 +11,6 @@ public class DemoController {
 
     @GetMapping("/hello")
     public String hello() throws UnknownHostException {
-        return "Hello sb " + InetAddress.getLocalHost().getHostName();
+        return "Hello ksb new branch" + InetAddress.getLocalHost().getHostName();
     }
 }
